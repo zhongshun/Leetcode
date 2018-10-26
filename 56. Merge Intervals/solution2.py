7f3f556e13ce91cc4e3d80ea92695ff9eb23fcaa
zhongshun <zszhang@outlook.com>
2018-10-26 17:48:42 -0400

    No.56 solution

--- 56. Merge Intervals/solution2.py
+++ 56. Merge Intervals/solution2.py
@@ -3,6 +3,19 @@ class Interval(object):
         self.start = s
         self.end = e
 
+def merge(intervals):
+    """
+    :type intervals: List[Interval]
+    :rtype: List[Interval]
+    """
+    intervals.sort(key=lambda i:i.start)
+    Meged = []
+    for s in intervals:
+        if Meged and Meged[-1].end >= s.start:
+            Meged[-1].end = max(s.end,Meged[-1].end)
+        else:
+            Meged += [s]
+    return Meged
 
 s1 = Interval(1,3)
 s2 = Interval(2,6)
@@ -10,6 +23,6 @@ s3 = Interval(8,10)
 s4 = Interval(15,18)
 s5 = Interval(4,7)
 intervals = [s1,s2,s3,s4]
-intervals.sort(key=lambda i:i.start)
-for i in range(4):
-    print(intervals[i].start,intervals[i].end)
+
+mege = merge(intervals)
+for i in range
