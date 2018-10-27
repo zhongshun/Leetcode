ed265ea638453f41b05e7fa377cde0b3b73cc651
zhongshun <zszhang@outlook.com>
2018-10-27 18:37:51 -0400

    No.58 solution 1

---
+++ 58. Length of Last Word/solution 1.py
@@ -0,0 +1,19 @@
+def lengthOfLastWord(s):
+    if not s:
+        return 0
+    res = 0
+    s_list = list(s)
+    while s_list[-1] == ' ':
+        s_list.pop()
+        if not s_list:
+            return 0
+
+    while s_list and s_list[-1] != ' ':
+        res = res + 1
+        s_list.pop()
+    return res
+
+s = "Hello"
+print(lengthOfLastWord(s))
+
+
