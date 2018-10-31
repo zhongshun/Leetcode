79b8f6034cd80ccc47478aad5f25f3d161f58707
zhongshun <zszhang@outlook.com>
2018-10-31 00:17:29 -0400

    140

--- 140. Word Break II/dp.py
+++ 140. Word Break II/dp.py
@@ -5,27 +5,27 @@ class Solution(object):
         :type wordDict: List[str]
         :rtype: List[str]
         """
+        #wordDict.sort(key=lambda i: len(i))
         return self.help(s,wordDict,{})
 
     def help(self,s,wordDict,memo):
-        if s in memo: return memo[s]
-        if not s: return []
-
+        if s in memo:
+            return memo[s]
+        if not s:
+            return []
         res = []
         for word in wordDict:
             if word != s[:len(word)]:
                 continue
-            if len(s) == len(word):
+            if not s[len(word):]:
                 res.append(word)
             else:
-                rest = self.help(s[len(word):],wordDict,memo)
-                for item in rest:
-                    item = word + ' ' + item
-                    res.append(item)
+                ans = self.help(s[len(word):],wordDict,memo)
+                for temp in ans:
+                    temp = word + ' ' + temp
+                    res.append(temp)
         memo[s] = res
         return res
-
-
 
 
 
